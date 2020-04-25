495990d17eac7513c6c1a4c8253bfd1199405ad0
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-04-25 15:12:18 -0500

    Upgrade Listeners

--- main.js
+++ main.js
@@ -1,81 +1,89 @@
 (function(){
-	let list = document.getElementById('list'),
+    let list = document.getElementById('list'),
 	taskInput = document.getElementById('taskInput'),
-	submit = document.getElementById('btn-add');
+	submit = document.getElementById('btn-add'),
+	form = document.getElementById('form');
 
-	// Functions
-	function addTask()
+    // Capture enter event from form tag
+    form.addEventListener('keypress', function(event){
+	if (event.key === 'Enter')
 	{
-		let task = taskInput.value,
-		newTask = document.createElement("li"),
-		link = document.createElement("a"),
-		content = document.createTextNode(task);
+	    event.preventDefault();
+	    addTask();
+	}
+    });
 
-		let check = lookFor(task);
+    // Functions
+    function addTask()
+    {
+	let task = taskInput.value,
+	    newTask = document.createElement("li"),
+	    link = document.createElement("a"),
+	    content = document.createTextNode(task);
 
-		if (check === false)
-		{
-			return (false);
-		}
+	checkInput();
 
-		if (task == "")
-		{
-			taskInput.className = "error";
-			return (false);
-		}
+	let check = lookFor(task);
 
-		// Add content to link
-		link.appendChild(content);
-		// Add attribute to link
-		link.setAttribute("href", "#");
+	if (check === false){ return (false); }
 
-		// Add link to newTask
-		newTask.appendChild(link);
-		// Add line separator to task
-		newTask.appendChild(document.createElement("hr"));
-
-		// Add new task
-		list.appendChild(newTask);
-
-		taskInput.value = "";
-
-
-		for (let i = 0; i <= list.children.length - 1; i++)
-		{
-			list.children[i].addEventListener("click", removeTask);
-		}
-	};
-
-	function checkInput()
+	if (task == "")
 	{
-		taskInput.className = "";
-	};
-
-	function lookFor(currentTask)
-	{
-		for (let i = 0; i <= list.children.length - 1; i++)
-		{
-			if (list.children[i].textContent === currentTask)
-			{
-				taskInput.value = "";
-				alert("This task already exists");
-				return (false);
-			}
-		}
-		return (true);
+	    taskInput.className = "error";
+	    return (false);
 	}
 
-	function removeTask()
+	// Add content to link
+	link.appendChild(content);
+	// Add attribute to link
+	link.setAttribute("href", "#");
+
+	// Add link to newTask
+	newTask.appendChild(link);
+	// Add line separator to task
+	newTask.appendChild(document.createElement("hr"));
+
+	// Add new task
+	list.appendChild(newTask);
+
+	taskInput.value = "";
+
+
+	for (let i = 0; i <= list.children.length - 1; i++)
 	{
-		this.parentNode.removeChild(this);
-	};
+	    list.children[i].addEventListener("click", removeTask);
+	}
+    };
 
-	// Events
+    function checkInput()
+    {
+	taskInput.className = "";
+    };
 
-	// Add Task
-	submit.addEventListener("click", addTask);
+    function lookFor(currentTask)
+    {
+	for (let i = 0; i <= list.children.length - 1; i++)
+	{
+	    if (list.children[i].textContent === currentTask)
+	    {
+		taskInput.value = "";
+		alert("This task already exists");
+		return (false);
+	    }
+	}
+    }
 
-	// Check Input
-	taskInput.addEventListener("click", checkInput);
+    function removeTask()
+    {
+	this.parentNode.removeChild(this);
+    };
+
+    // Events
+
+    // Add Task
+    submit.addEventListener("click", addTask);
+
+    // Check Input
+    taskInput.addEventListener("click", checkInput);
 
 }())
