2d94eef00a34e03b42bf714b52b59e4ecf393962
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-08-01 01:28:04 -0500

    Remove Task Added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# TODO-LIST
+# :rocket: TODO-LIST :rocket:
+
+This is a TODO using Javascript Vanilla. It's an application made only using knowledge acquired so far.
+
+## :boom: What u can do :boom:
+
+- Add a new task
+- Remove a task
+
+Not a big application but it's just for learning :)
--- scripts/main.js
+++ scripts/main.js
@@ -24,21 +24,19 @@ function add_task(task, container)
 	text.appendChild(document.createElement('p'));
 	text.children[0].textContent = task.value;
 
-	// ** Add trash icon **
-	//text.append(document.createElement('button'));
-	//text.children[1].setAttribute('class', 'trash');
-	//text.children[1].appendChild(document.createElement('i'));
-	//text.children[1].children[0].setAttribute('class', 'fa fa-minus-square');
+	text.addEventListener('click', (e) => {
+
+	    text.classList.toggle('deleted');
+	    setTimeout(() => {
+		text.remove()
+	    }, 300);
+
+	})
 
 	// ** Add new task to the container **
 	container.prepend(text);
 
     }
-    else
-    {
-	;
-    }
-
 
     return (output);
 }
@@ -52,5 +50,6 @@ function add_task(task, container)
 	e.preventDefault();
 	text_box.value = add_task(text_box, todos_box);
     });
+
 }
 )();
